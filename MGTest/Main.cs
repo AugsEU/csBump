68e83758909ec95c07c8b1cd29314797ac5fa034
August Dahlkvist <AugustDahlkvist@gmail.com>
2024-11-29 17:17:21 +0100

    Add monogame test project

---
+++ MGTest/Main.cs
@@ -0,0 +1,78 @@
+﻿using csBump;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MGTest
+{
+	public class Main : Game
+	{
+		static Texture2D mBlankTexture;
+
+		private GraphicsDeviceManager mGraphics;
+		private SpriteBatch mSpriteBatch;
+
+		World mWorld;
+
+		MovingRect mPlayer;
+		MovingRect mOtherPlayer;
+
+		public Main()
+		{
+			mGraphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			mBlankTexture = new Texture2D(mGraphics.GraphicsDevice, 1, 1);
+			mBlankTexture.SetData<Color>(new Color[] { Color.White} );
+
+			mWorld = new World();
+
+			mPlayer = new MovingRect(mWorld, new Vector2(10.0f, 10.0f), new Vector2(16.0f, 16.0f));
+			mOtherPlayer = new MovingRect(mWorld, new Vector2(30.0f, 10.0f), new Vector2(16.0f, 16.0f));
+			mOtherPlayer.mUpKey = Keys.Up;
+			mOtherPlayer.mDownKey = Keys.Down;
+			mOtherPlayer.mLeftKey = Keys.Left;
+			mOtherPlayer.mRightKey = Keys.Right;
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			mSpriteBatch = new SpriteBatch(GraphicsDevice);
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			mPlayer.Update(gameTime, mWorld);
+			mOtherPlayer.Update(gameTime, mWorld);
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+			mSpriteBatch.Begin();
+
+			mPlayer.Draw(mSpriteBatch);
+			mOtherPlayer.Draw(mSpriteBatch);
+
+			mSpriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+
+		public static void DrawRect(SpriteBatch sb, Vector2 pos, Vector2 size, Color color)
+		{
+			Rectangle intRect = new Rectangle((int)pos.X, (int)pos.Y, (int)size.X, (int)size.Y);
+
+			sb.Draw(mBlankTexture, intRect, color);
+		}
+	}
+}
---
+++ MGTest/MovingRect.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using csBump;
+using static csBump.IResponse;
+
+namespace MGTest
+{
+	internal class MovingRect
+	{
+		const float SPEED = 50.2f;
+
+		Vector2 mPosition;
+		Vector2 mSize;
+
+		public Item mBumpItem;
+		public Color mColor = Color.Gray;
+
+		public Keys mUpKey = Keys.W;
+		public Keys mDownKey = Keys.S;
+		public Keys mLeftKey = Keys.A;
+		public Keys mRightKey = Keys.D;
+
+		public MovingRect(World bumpWorld, Vector2 position, Vector2 size)
+		{
+			mPosition = position;
+			mSize = size;
+
+			mBumpItem = new Item();
+			bumpWorld.Add(mBumpItem, mPosition.X, mPosition.Y, mSize.X, mSize.Y);
+		}
+
+		public void Update(GameTime gameTime, World bumpWorld)
+		{
+			float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+			Vector2 desirePos = mPosition;
+			KeyboardState keyState = Keyboard.GetState();
+			if(keyState.IsKeyDown(mUpKey))
+			{
+				desirePos += new Vector2(0.0f, -1.0f) * SPEED * dt;
+			}
+			if (keyState.IsKeyDown(mDownKey))
+			{
+				desirePos += new Vector2(0.0f, 1.0f) * SPEED * dt;
+			}
+			if (keyState.IsKeyDown(mLeftKey))
+			{
+				desirePos += new Vector2(-1.0f, 0.0f) * SPEED * dt;
+			}
+			if (keyState.IsKeyDown(mRightKey))
+			{
+				desirePos += new Vector2(1.0f, 0.0f) * SPEED * dt;
+			}
+
+			Result result = bumpWorld.Move(mBumpItem, desirePos.X, desirePos.Y, new DefaultFilter());
+
+			Rect rect = bumpWorld.GetRect(mBumpItem);
+			mPosition.X = rect.mX;
+			mPosition.Y = rect.mY;
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			Main.DrawRect(spriteBatch, mPosition, mSize, mColor);
+		}
+	}
+}
